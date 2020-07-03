4902d5f11376b0b85c53eb65a9ef04ed6a4b31f4
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2020-07-03 10:34:20 -0300

    Módulo Moser usado para geração gráfica
    Módulo adaptado para permitir a entrada manual e importação automatizada de diferentes valores referentes à constante adimensional de Moser "u" e, com isso, a geração de figura gráfica ilustrativa da sua influência sobre a velocidade do crescimento microbiano, em termos da taxa µ.

---
+++ Graficos_Documentos_Submetidos/Modulos_Moser.py
@@ -0,0 +1,85 @@
+                                 # MODELO CINÉTICO DE CRESCIMENTO MICROBIANO SEM INIBIÇÃO - MOSER #
+
+# Módulos com as funções de: 
+# 1) Entrada dos valores iniciais dos parâmetros cinéticos, concentrações iniciais e tempo total de cultivo;
+# 2) Importação dos dados experimentais de Cx, Cs e Cp diretamente de arquivos .xlsx;
+# 3) Modelagem (parâmetros cinéticos passam a ser argumentos na função de integração numérica)
+
+# Importação dos pacotes:
+import numpy as np
+import pandas as pd
+
+# Função 1)
+def entr_rand_Moser():
+    def entr_rand_Moser_gerand():
+        mimaximo = 0.35 # unidade 1/hora - taxa específica de crescimento
+        Ks = 9.8 # unidade g/L - constante de semi-saturação
+        Kd = 0.0146  #unidade de 1/h - constante de morte celular
+        Cx0 = 0.85  # unidade g/L - concentração inicial de microrganismo
+        Cs0 = 52 # unidade g/L - concentração inicial de substrato
+        Cp0 = 0  # unidade g/L - concentração inicial de produto
+        tf =  42 # unidade horas - tempo final da integração
+        Yxs = 0.65 #unidade g células/g substrato - coeficiente estequiométrico
+        alfa = 0.14 # unidade g células/g produto - coeficiente estequiométrico
+        beta = 0 # unidade g células/g produto . h - coeficiente estequiométrico
+        mi_exp_1 = 0.6
+        mi_exp_2 = 0.8
+        mi_exp_3 = 1.0
+        mi_exp_4 = 1.6
+        mi_exp_5 = 1.8
+        return(mimaximo, Ks, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta, mi_exp_1, mi_exp_2, mi_exp_3, mi_exp_4, mi_exp_5)
+    mimaximo, Ks, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta, mi_exp_1, mi_exp_2, mi_exp_3, mi_exp_4, mi_exp_5 = entr_rand_Moser_gerand()
+    entr_rand_val = [mimaximo, Ks, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta, mi_exp_1, mi_exp_2, mi_exp_3, mi_exp_4, mi_exp_5]
+    cond_inic = [Cx0,Cs0,Cp0]
+    t = np.arange(0,tf,1.5)
+    return(entr_rand_val, cond_inic, t)
+
+# Função 2)
+def modelag_bat_Moser_dados_conc_sim():
+   importado = pd.read_excel("Dados_entrada_bat_Moser_modelagem_01.xlsx","C_exp") 
+   importado_np = importado.values
+   t_exp = importado_np[:,0]
+   Cx_exp = importado_np [:,1]
+   Cs_exp = importado_np [:,2]
+   Cp_exp = importado_np [:,3]
+   C_exp = np.zeros((len(t_exp),3))
+   C_exp[:,0] = Cx_exp
+   C_exp[:,1] = Cs_exp
+   C_exp[:,2] = Cp_exp
+   return(C_exp, t_exp)
+
+# Função 3)
+def modelag_bat_Moser_func_args():
+    def bat_Moser(C, t, *args):
+        mimaximo = args[0]
+        Ks = args[1]
+        Kd = args[2]
+        Yxs = args[3]
+        alfa = args[4]
+        beta = args[5]
+        mi_exp = args[6]
+    
+        mi=mimaximo*(((C[1])**mi_exp)/(Ks+((C[1])**mi_exp)))
+        dCxdt=(mi-Kd)*C[0]
+        dCsdt=(-1/Yxs)*mi*C[0]
+        dCpdt=alfa*mi*C[0]+beta*C[0]
+        return(dCxdt,dCsdt,dCpdt)
+    return(bat_Moser)
+
+# Função 4)
+def simul_bat_Moser():
+    def edos_int_bat_Moser(C,t):
+        Cx,Cs,Cp=C
+        mimax_sim = mimax
+        Ks_sim = Ks
+        Kd_sim = Kd
+        Yxs_sim = Yxs
+        alfa_sim = alfa
+        beta_sim = beta
+        u_sim = u
+        
+        mi=mimax_sim*(((Cs)**u_sim)/(Ks_sim+((Cs)**u_sim)))
+        dCxdt=(mi-Kd_sim)*Cx
+        dCsdt=(-1/Yxs_sim)*mi*Cx
+        dCpdt=alfa_sim*mi*Cx+beta_sim*Cx
+        return(dCxdt,dCsdt,dCpdt)
