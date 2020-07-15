6d29293b1e610a19ba53bdb8854eb3bee66a0f49
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2020-07-15 16:46:38 -0300

    Correção comentário

--- Simulacao_Batelada_Modelos_Cineticos/Inibicao_pelo_Substrato/Simulacao_Modelo_Wu_et_al.py
+++ Simulacao_Batelada_Modelos_Cineticos/Inibicao_pelo_Substrato/Simulacao_Modelo_Wu_et_al.py
@@ -1,4 +1,4 @@
-# Importação das bibliotecas necessárias para as partes não modulares:
+# Importação das bibliotecas e módulos necessários:
 import Modulos_Wu_et_al
 import Modulos_configuracao_graficos
 from scipy.integrate import odeint
