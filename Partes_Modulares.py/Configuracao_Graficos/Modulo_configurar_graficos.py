d74fdacd29579cffc9afefde7bb92568f5b7521d
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2020-07-15 16:42:42 -0300

    Módulo com funções para padronização de gráficos

---
+++ Partes_Modulares.py/Configuracao_Graficos/Modulo_configurar_graficos.py
@@ -0,0 +1,31 @@
+                                 # GERAÇÃO DE GRÁFICOS - ADOTADA PARA CÓDIGOS DE SIMULAÇÃO E MODELAGEM #
+
+# Módulos com as funções de: 
+# 1) Definição do tamanho da fonte dos eixos;
+# 2) Padronização das dimensões e background das figuras geradas.
+
+# Importação dos pacotes:
+import matplotlib.pyplot as plt
+
+# Função 1)
+def config_plot():
+    SMALL_SIZE = 13                       
+    MEDIUM_SIZE = 16                     
+    BIGGER_SIZE = 16
+    plt.rc('font', size=SMALL_SIZE)          
+    plt.rc('axes', titlesize=SMALL_SIZE)     
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    
+    plt.rc('xtick', labelsize=SMALL_SIZE)    
+    plt.rc('ytick', labelsize=SMALL_SIZE)    
+    plt.rc('legend', fontsize=SMALL_SIZE)    
+    plt.rc('figure', titlesize=BIGGER_SIZE)  
+    return(config_plot)
+
+# Função 2)
+def config_estetica_eixo_unico():
+     f = plt.figure()              
+     f.set_figheight(5)                                                 
+     f.set_figwidth(8)                                                  
+     f.patch.set_facecolor('white')                                     
+     plt.style.use('default') 
+     return(config_estetica_eixo_unico)
