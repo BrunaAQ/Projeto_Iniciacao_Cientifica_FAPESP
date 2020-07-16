241341a7fb9c4b1e08cdacc73a1ef07e5a6de56d
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2020-07-16 16:18:41 -0300

    Melhoria nos comentários

--- Partes_Modulares.py/Ausencia_de_Inibicao/Modulos_Contois.py
+++ Partes_Modulares.py/Ausencia_de_Inibicao/Modulos_Contois.py
@@ -12,16 +12,16 @@ import pandas as pd
 # Função 1)
 def entr_rand_Contois():
     def entr_rand_Contois_gerand():
-        mimaximo = 0.7 # unidade 1/hora - taxa específica de crescimento
-        KSX = 7.44 # unidade g/L - constante de semi-saturação
-        Kd = 0.0146  #unidade de 1/h - constante de morte celular
+        mimaximo = 0.7 # unidade 1/h - taxa específica de crescimento
+        KSX = 7.44 # unidade g substrato/g células - constante de semi-saturação (constante de Contois)
+        Kd = 0.0146  # unidade de 1/h - constante de morte celular
         Cx0 = 0.85  # unidade g/L - concentração inicial de microrganismo
         Cs0 = 40 # unidade g/L - concentração inicial de substrato
         Cp0 = 0  # unidade g/L - concentração inicial de produto
         tf =  32 # unidade horas - tempo final da integração
         Yxs = 0.5826 #unidade g células/g substrato - coeficiente estequiométrico
-        alfa = 0.7778 # unidade g células/g produto - coeficiente estequiométrico
-        beta = 0 # unidade g células/g produto . h - coeficiente estequiométrico
+        alfa = 0.7778 # unidade g produto/g células - coeficiente estequiométrico
+        beta = 0 # unidade g produto/g células . h - coeficiente estequiométrico
         return(mimaximo, KSX, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta)
     mimaximo, KSX, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta = entr_rand_Contois_gerand()
     entr_rand_val = [mimaximo, KSX, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta]
@@ -31,12 +31,13 @@ def entr_rand_Contois():
 
 # Função 2)
 def modelag_bat_Contois_dados_conc_sim():
-   importado = pd.read_excel("C_exp_rand_sim_bat_Contois_ref_03.xlsx","C_t_exp") 
+## Escrever o nome do arquivo e da planilha; o mesmo deve estar na mesma pasta que o arquivo .py:
+   importado = pd.read_excel("Contois_bat_relat_fapesp.xlsx","C_t_exp") 
    importado_np = importado.values
-   t_exp = importado_np[:,0]
-   Cx_exp = importado_np [:,1]
-   Cs_exp = importado_np [:,2]
-   Cp_exp = importado_np [:,3]
+   t_exp = importado_np[:,1]
+   Cx_exp = importado_np [:,2]
+   Cs_exp = importado_np [:,3]
+   Cp_exp = importado_np [:,4]
    C_exp = np.zeros((len(t_exp),3))
    C_exp[:,0] = Cx_exp
    C_exp[:,1] = Cs_exp
@@ -60,20 +61,4 @@ def modelag_bat_Contois_func_args():
         return(dCxdt,dCsdt,dCpdt)
     return(bat_Contois)
 
-# Função 4)
-def simul_bat_Contois():
-    def edos_int_bat_Contois(C,t):
-        Cx,Cs,Cp=C
-        mimax_sim = mimax
-        KSX_sim = KSX
-        Kd_sim = Kd
-        Yxs_sim = Yxs
-        alfa_sim = alfa
-        beta_sim = beta
-        
-        mi=mimax_sim*(Cs/((KSX_sim*Cx)+Cs))
-        dCxdt=(mi-Kd_sim)*Cx
-        dCsdt=(-1/Yxs_sim)*mi*Cx
-        dCpdt=alfa_sim*mi*Cx+beta_sim*Cx
-        return(dCxdt,dCsdt,dCpdt)
-    return(edos_int_bat_Contois)
+
