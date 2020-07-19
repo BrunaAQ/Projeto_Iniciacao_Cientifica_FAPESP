4bc0189e0e2ac2b485352828eda9e174630d0ce6
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2020-07-19 11:26:05 -0300

    Melhoria nos comentários

--- Partes_Modulares.py/Inibicao_pelo_Substrato/Modulos_Wu_et_al.py
+++ Partes_Modulares.py/Inibicao_pelo_Substrato/Modulos_Wu_et_al.py
@@ -1,4 +1,4 @@
-                     # MODELO CINÉTICO DE CRESCIMENTO MICROBIANO COM INIBIÇÃO (SUBSTRATO) - WU et al #
+                     # MODELO CINÉTICO DE CRESCIMENTO MICROBIANO COM INIBIÇÃO (SUBSTRATO) - WU et al. #
 
 # Módulos com as funções de: 
 # 1) Entrada dos valores iniciais dos parâmetros cinéticos, concentrações iniciais e tempo total de cultivo;
@@ -8,23 +8,22 @@
 # Importação dos pacotes:
 import numpy as np
 import pandas as pd
-import random 
 
 # Função 1)
 def entr_rand_Wu_colab():
     def entr_rand_Wu_colab_gerand():
-        mimaximo = 0.45 #unidade 1/hora - taxa específica de crescimento
-        Ks = 0.45 #unidade g/L - constante de semi-saturação
-        Kd = 0.021  #unidade de 1/h - constante de morte celular
+        mimaximo = 0.45 # unidade 1/hora - taxa específica de crescimento
+        Ks = 3.14 # unidade g/L - constante de semi-saturação
+        Kd = 0.021  # unidade de 1/h - constante de morte celular
         Cx0 = 1.5 # unidade g/L - concentração inicial de microrganismo
         Cs0 = 100  # unidade g/L - concentração inicial de substrato
         Cp0 = 0  # unidade g/L - concentração inicial de produto
         tf = 32 # unidade horas - tempo final da integração
-        Yxs = 0.6 #unidade g células/g substrato - coeficiente estequiométrico
-        alfa = 0.05 # unidade g células/g produto - coeficiente estequiométrico
-        beta = 0
-        KE = 25
-        v = 1
+        Yxs = 0.6 # unidade g células/g substrato - coeficiente estequiométrico
+        alfa = 0.05 # unidade g produto/g células- coeficiente estequiométrico
+        beta = 0 # unidade g produto/g células . h- coeficiente estequiométrico
+        KE = 25 # unidade g/L - constante de inibição
+        v = 1 # adimensional - termo expoente
         return(mimaximo, Ks, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta, KE, v)
     mimaximo, Ks, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta, KE, v = entr_rand_Wu_colab_gerand()
     entr_rand_val = [mimaximo, Ks, Kd, Cx0, Cs0, Cp0, tf, Yxs, alfa, beta, KE, v]
@@ -34,12 +33,13 @@ def entr_rand_Wu_colab():
 
 # Função 2)
 def modelag_bat_Wu_et_al_dados_conc_sim():
-   importado = pd.read_excel("Dados_conc_sim_bat_Wu_et_al_modelag.xlsx","C_exp") 
+## Digitar o nome do arquivo acompanhado do da planilha:
+   importado = pd.read_excel("Wu_bat_gerado_pa_ig_Ki_25_n2_novo.xlsx","C_t_exp") 
    importado_np = importado.values
-   t_exp = importado_np[:,0]
-   Cx_exp = importado_np [:,1]
-   Cs_exp = importado_np [:,2]
-   Cp_exp = importado_np [:,3]
+   t_exp = importado_np[:,1]
+   Cx_exp = importado_np [:,2]
+   Cs_exp = importado_np [:,3]
+   Cp_exp = importado_np [:,4]
    C_exp = np.zeros((len(t_exp),3))
    C_exp[:,0] = Cx_exp
    C_exp[:,1] = Cs_exp
@@ -64,29 +64,3 @@ def modelag_bat_Wu_et_al_func_args():
         dCpdt=alfa*mi*C[0]+beta*C[0]
         return(dCxdt,dCsdt,dCpdt)
     return(bat_Wu_colab)
-
-# Função 4)
-def simul_bat_Wu_et_al():
-    def edos_int_bat_Wu_et_al(C,t):
-        Cx,Cs,Cp=C
-        mimax_sim = mimax
-        Ks_sim = Ks
-        Kd_sim = Kd
-        Yxs_sim = Yxs
-        alfa_sim = alfa
-        beta_sim = beta
-        Ke_sim = Ke
-        v_sim = v
-
-        mi = mimax_sim * (Cs/(Ks_sim + Cs + Cs*((Cs/Ke_sim)**v_sim)))
-        dCxdt=(mi-Kd_sim)*Cx
-        dCsdt=(-1/Yxs_sim)*mi*Cx
-        dCpdt=alfa_sim*mi*Cx+beta_sim*Cx
-        return(dCxdt,dCsdt,dCpdt)
-
-
-
-
-
-
-
