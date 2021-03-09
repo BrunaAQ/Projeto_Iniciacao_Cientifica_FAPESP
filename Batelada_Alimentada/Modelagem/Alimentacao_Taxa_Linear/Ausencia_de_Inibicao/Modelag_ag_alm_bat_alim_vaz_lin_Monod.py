365fd4f843c1900584ad6ba5e59377d0289edadc
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2021-03-09 14:36:13 -0300

    Correção equação linear

--- Batelada_Alimentada/Modelagem/Alimentacao_Taxa_Linear/Ausencia_de_Inibicao/Modelag_ag_alm_bat_alim_vaz_lin_Monod.py
+++ Batelada_Alimentada/Modelagem/Alimentacao_Taxa_Linear/Ausencia_de_Inibicao/Modelag_ag_alm_bat_alim_vaz_lin_Monod.py
@@ -8,7 +8,7 @@ Created on Sat Oct 31 11:16:08 2020
                          ## MODELAGEM BATELADA ALIMENTADA VAZÃO LINEAR CONSTANTE CINÉTICA CLÁSSICA ##
 
 # Importação das bibliotecas necessárias para as partes não modulares:
-import Modulos_Monod_bat_alim
+import Modulos_Monod_bat_alim_sem_Kd
 import Modulos_configuracao_graficos
 import Modulo_peso_limite_AG
 from scipy.integrate import odeint
@@ -26,7 +26,7 @@ import time
      #*ETAPA 1*# - BATELADA
 ## Módulos:
 ### Valores dos parâmetros do modelo e condição inicial:
-dad_entr_geral = Modulos_Monod_bat_alim.entr_Monod()
+dad_entr_geral = Modulos_Monod_bat_alim_sem_Kd.entr_Monod()
 ## Valor de entrada dos parâmetros cinéticos
 pars_entr = dad_entr_geral[0]
 mimaximo = pars_entr[0]
@@ -63,7 +63,7 @@ a = param_oper_alim[6]
 def bat_alim_Monod (Concent,t_exp_alim):
     Cx,Cs,Cp=Concent
     mi = mimaximo*(Cs/(Ks+Cs))
-    D = (Q0*(1+a*t_exp_alim))/((Q0*(t_exp_alim+(a*t_exp_alim**2)))+V0)
+    D = (Q0*(1+a*t_exp_alim))/((Q0*(t_exp_alim+(a*((t_exp_alim**2)/2))))+V0)
     dCxdt = (mi-D)*Cx
     dCsdt = D*(Cs0_corrent_alim-Cs)-((mi*Cx)/Yxs)
     dCpdt = D*(Cp0_alim-Cp)+Cx*(beta+alfa*mi)
@@ -87,7 +87,7 @@ start_tempo = time.time()
   ##*Algoritmo Genético (global)*##
 # Módulos
 ## Função com as equações modelo com os parâmetros atribuídos a argumentos:
-func_args_bat = Modulos_Monod_bat_alim.modelag_bat_Monod_func_args()
+func_args_bat = Modulos_Monod_bat_alim_sem_Kd.modelag_bat_Monod_func_args()
 ## Atribuição de pesos a Cx, Cs e Cp para a modelagem (tendência de convergência - ideia de prioridade):
 dpC = Modulo_peso_limite_AG.peso()
 ## Função objetiva, compara os pontos experimentais com o sistema cinético adotado:
@@ -152,7 +152,7 @@ def func_args_alim(C, t_exp_alim, *args):
     beta = args[4]
         
     mi = mimaximo*(C[1]/(Ks+C[1]))
-    D = (Q0*(1+a*t_exp_alim))/((Q0*(t_exp_alim+(a*t_exp_alim**2)))+V0)
+    D = (Q0*(1+a*t_exp_alim))/(Q0*(t_exp_alim+(a*((t_exp_alim**2)/2)))+V0)
     dCxdt = (mi - D)*C[0]
     dCsdt = D*(Cs0_corrent_alim - C[1]) - ((mi*C[0])/Yxs)
     dCpdt = D*(Cp0_alim - C[2]) + C[0]*(beta + alfa*mi)
@@ -541,20 +541,3 @@ _ = plt.style.use('default')
 _ = f.set_figheight(9)                                                 
 _ = f.set_figwidth(14)
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
