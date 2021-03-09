804b1104d1cb960e0fbece9080be7988734c234b
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2021-03-09 14:45:44 -0300

    Correção equação linear

--- Batelada_Alimentada/Modelagem/Alimentacao_Taxa_Linear/Inibicao_pelo_Substrato/Modelag_ag_alm_bat_alim_vaz_lin_Andrews.py
+++ Batelada_Alimentada/Modelagem/Alimentacao_Taxa_Linear/Inibicao_pelo_Substrato/Modelag_ag_alm_bat_alim_vaz_lin_Andrews.py
@@ -7,7 +7,7 @@ Created on Thu Nov 19 11:05:33 2020
                     ## MODELAGEM BATELADA ALIMENTADA À VAZÃO LINEAR CINÉTICA DE ANDREWS ##
 
 # Importação das bibliotecas necessárias para as partes não modulares:
-import Modulos_Andrews_bat_alim
+import Modulos_Andrews_bat_alim_sem_Kd
 import Modulo_peso_limite_AG
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
@@ -24,7 +24,7 @@ import time
         #*ETAPA 1*# - BATELADA
 ## Módulos:
 ### Valores dos parâmetros do modelo e condição inicial:
-dad_entr_geral = Modulos_Andrews_bat_alim.entr_Andrews()
+dad_entr_geral = Modulos_Andrews_bat_alim_sem_Kd.entr_Andrews()
 ## Valor de entrada dos parâmetros cinéticos
 pars_entr = dad_entr_geral[0]
 mimaximo = pars_entr[0]
@@ -63,7 +63,7 @@ def bat_alim_Andrews(Concent,t_exp_alim):
     a = param_oper_alim[6]
     Cx,Cs,Cp = Concent
     mi = mimaximo*(Cs/(Ks + Cs + ((Cs**2)/KIS)))
-    D = (Q0*(1 + a*t_exp_alim))/((Q0*(t_exp_alim + (a*t_exp_alim**2))) + V0)
+    D = (Q0*(1 + a*t_exp_alim))/((Q0*(t_exp_alim + (a*((t_exp_alim**2)/2)))) + V0)
     dCxdt = (mi-D)*Cx
     dCsdt = D*(Cs0_corrent_alim-Cs)-((mi*Cx)/Yxs)
     dCpdt = D*(Cp0_alim-Cp)+Cx*(beta+alfa*mi)
@@ -88,7 +88,7 @@ start_tempo = time.time()
   ##*Algoritmo Genético (global)*##
 # Módulos
 ## Função com as equações modelo com os parâmetros atribuídos a argumentos:
-func_args_bat = Modulos_Andrews_bat_alim.modelag_bat_Andrews_func_args()
+func_args_bat = Modulos_Andrews_bat_alim_sem_Kd.modelag_bat_Andrews_func_args()
 ## Atribuição de pesos a Cx, Cs e Cp para a modelagem (tendência de convergência - ideia de prioridade):
 dpC = Modulo_peso_limite_AG.peso()
 ## Função objetiva, compara os pontos experimentais com o sistema cinético adotado:
@@ -156,7 +156,7 @@ def func_args_alim(C, t_exp_alim, *args):
     a = param_oper_alim[6]
         
     mi = mimaximo*(C[1]/(Ks + C[1] + ((C[1]**2)/KIS)))
-    D = (Q0*(1 + a*t_exp_alim))/((Q0*(t_exp_alim + (a*t_exp_alim**2))) + V0)
+    D = (Q0*(1 + a*t_exp_alim))/((Q0*(t_exp_alim + (a*((t_exp_alim**2)/2)))) + V0)
     dCxdt = (mi - D)*C[0]
     dCsdt = D*(Cs0_corrent_alim - C[1]) - ((mi*C[0])/Yxs)
     dCpdt = D*(Cp0_alim - C[2]) + C[0]*(beta + alfa*mi)
@@ -501,6 +501,3 @@ f.set_figwidth(14)
 f.patch.set_facecolor('white')                                   
 plt.style.use('default')                       
 plt.show() 
-
-
-
