32c5dae30d81376b3670822f7fa89fdf3f372cbc
BrunaAQ <49010864+BrunaAQ@users.noreply.github.com>
2021-03-09 14:25:58 -0300

    Ligeira correção na equação de aplicação

--- Batelada_Alimentada/Simulacao/Alimentacao_Taxa_Linear/Inibicao_pelo_Produto/Simul_bat_alim_vaz_lin_Aiba_et_al.py
+++ Batelada_Alimentada/Simulacao/Alimentacao_Taxa_Linear/Inibicao_pelo_Produto/Simul_bat_alim_vaz_lin_Aiba_et_al.py
@@ -118,7 +118,7 @@ def modeloscrescimentoalimentada (Concent,tal):
     Cx,Cs,Cp = Concent
     mult_exp = -Kp*Cp
     mi = mimaximo*((Cs/(Ks+Cs))*np.exp(mult_exp))
-    D = (Q0*(1 + a*tal))/((Q0*(tal+(a*tal**2))) + V0)
+    D = (Q0*(1 + a*tal))/((Q0*(tal+((a*tal**2)/2))) + V0)
     dCxdt = (mi-D)*Cx
     dCsdt = D*(Cs0alimentacao-Cs)-((mi*Cx)/Yxs)
     dCpdt = D*(Cp0ba-Cp)+Cx*(beta+alfa*mi)
@@ -304,7 +304,7 @@ plt.show()
 t_ba = Tfba - Tfbatelada
 t_total_ba = np.arange(Tfbatelada, t_ba, Intervaloba)
 ## Cálculo volume(t) - integração dV/dt = Q para descrito pela equação linear:
-V_calc = (Q0*(t_total_ba + (a*t_total_ba**2))) + V0
+V_calc = (Q0*(t_total_ba + (a*((t_total_ba**2)/2)))) + V0
 
 ## Plotando a figura gráfica - variação do volume do tanque: 
 tam_graf()   
@@ -337,18 +337,3 @@ _ = f.set_figheight(9)
 _ = f.set_figwidth(14) 
 _ = f.patch.set_facecolor('white')                                   
 _ = plt.style.use('default')    
-
-
-
-D = (Q0*(1 + a*tal))/((Q0*(tal+(a*tal**2))) + V0)
-
-## Tempo de processo em batelada alimentada:
-t_ba = Tfba - Tfbatelada
-t_total_ba = np.arange(Tfbatelada, t_ba, Intervaloba)
-## Cálculo volume(t) - integração dV/dt = Q para descrito pela equação linear:
-V_calc = (Q0*(t_total_ba + (a*t_total_ba**2))) + V0
-
-## Variação linear temporal da vazão de alimentação:
-### Função Q(t) original:
-Q_calc = Q0*(1 + a*t_total_ba)
-
